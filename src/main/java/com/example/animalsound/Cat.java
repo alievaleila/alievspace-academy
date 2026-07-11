8793d21521fe0079c7b23e94af2d7842effa3c3e
alievaleila <meliyeva452@gmail.com>
2026-07-11 22:19:24 +0400

    feat: implement Payment System with Payable and Refundable interfaces

---
+++ src/main/java/com/example/animalsound/Cat.java
@@ -0,0 +1,9 @@
+package com.example.animalsound;
+
+public class Cat implements Sound {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Miyov");
+    }
+}
---
+++ src/main/java/com/example/animalsound/Sound.java
@@ -0,0 +1,7 @@
+package com.example.animalsound;
+
+public interface Sound {
+
+    void makeSound();
+
+}
